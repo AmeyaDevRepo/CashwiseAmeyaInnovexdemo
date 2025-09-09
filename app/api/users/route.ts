4a06b06ba75d5b5b45e68966b434b5e5b95fbb6e
AmeyaDevRepo <134253316+AmeyaDevRepo@users.noreply.github.com>
2025-09-09 11:13:42 +0530

    remove type to create user

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,142 +2,174 @@ export const maxDuration = 60;
 import DB from "@app/_database/db";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt";
-import { toast } from 'react-toastify';
-import hash from 'bcrypt';
+import { toast } from "react-toastify";
+import hash from "bcrypt";
 import Users from "@app/_model/user.model";
 
 // Initialize database connection
 (async function initializeDB() {
-    await DB();
-  })();
+  await DB();
+})();
 
-
-
-  // email validation function
-  function validateEmail(email:string) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
+// email validation function
+function validateEmail(email: string) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
 // function to create new user by admin
 export async function POST(req: NextRequest) {
-    const userHeader = req.headers.get("user");
-        const authUser: any = userHeader ? JSON.parse(userHeader) : null;
-        if(authUser?.role!=="admin" && authUser?.role!=="manager"){
-            return NextResponse.json({
-                type: "BAD_REQUEST",
-                message: "You are not authorized to create user",
-            }, { status: 403 });
-        }
-    try {
-        // get the request body     
-        const {_id,name, email, password,role,phone,type } = await req.json();
-        // check if all fields are provided
-        if (!name || !name.trim(" ")  || !role || !type ) {
-            // error message handle
-            return NextResponse.json({
-                type: "BAD_REQUEST",
-                message: "All fields are required !",
-            }, { status: 400 });
-        }
-        // phone number validation
-            // Validate the phone numbers
+  const userHeader = req.headers.get("user");
+  const authUser: any = userHeader ? JSON.parse(userHeader) : null;
+  if (authUser?.role !== "admin" && authUser?.role !== "manager") {
+    return NextResponse.json(
+      {
+        type: "BAD_REQUEST",
+        message: "You are not authorized to create user",
+      },
+      { status: 403 }
+    );
+  }
+  try {
+    // get the request body
+    const { _id, name, email, password, role, phone, type } = await req.json();
+    // check if all fields are provided
+    if (!name || !name.trim(" ") || !role) {
+      // error message handle
+      return NextResponse.json(
+        {
+          type: "BAD_REQUEST",
+          message: "All fields are required !",
+        },
+        { status: 400 }
+      );
+    }
+    // phone number validation
+    // Validate the phone numbers
     if (phone.toString().length !== 10) {
+      return NextResponse.json(
+        { type: "BAD_REQUEST", message: "Phone number should be 10 digits." },
+        { status: 400 }
+      );
+    }
+    // email validation
+    if (email && !validateEmail(email)) {
+      return NextResponse.json(
+        {
+          type: "BAD_REQUEST",
+          message: "Invalid email address",
+        },
+        { status: 400 }
+      );
+    }
+    let hashedPassword: any = "";
+    if (password) {
+      hashedPassword = await bcrypt.hash(password, 10);
+    }
+    // check if user not exists
+    if (!_id) {
+      const user = new Users({
+        name,
+        email: email ?? "",
+        password: password ? hashedPassword : "",
+        role,
+        phone,
+      });
+      const result = await user.save();
+      return NextResponse.json(
+        {
+          type: "SUCCESS",
+          message: "User created successfully",
+          result,
+        },
+        { status: 201 }
+      );
+    } else {
+      const user = await Users.updateOne(
+        { _id },
+        { name, email, password: hashedPassword, role, phone },
+        { new: true }
+      );
+      if (!user) {
         return NextResponse.json(
-          {type: "BAD_REQUEST",
-            message: "Phone number should be 10 digits." },
-          { status: 400 }
+          {
+            type: "BAD_REQUEST",
+            message: "Failed to updated user",
+          },
+          { status: 200 }
         );
       }
-      // email validation
-        if (email && !validateEmail(email)) {
-            return NextResponse.json({
-                type: "BAD_REQUEST",
-                message: "Invalid email address",
-            }, { status: 400 });
-        }
-        let hashedPassword:any="";
-        if(password){
-        hashedPassword = await bcrypt.hash(password, 10);
-        }
-        // check if user not exists
-        if(!_id){
-            const user = new Users({
-                name,
-                email:email??"",
-                password: password?hashedPassword:"",
-                role,
-                phone,
-                type
-            });
-          const result = await user.save();
-            return NextResponse.json({
-                type: "SUCCESS",
-                message: "User created successfully",
-                result
-            }, { status: 201 });
-        }
-        else{
-            const user = await Users.updateOne({_id},{name,email,password:hashedPassword,role,phone,type},{new:true});
-           if(!user){
-            return NextResponse.json({
-                type: "BAD_REQUEST",
-                message: "Failed to updated user",
-            }, { status: 200 });
-           }
-            return NextResponse.json({
-                type: "SUCCESS",
-                message: "User updated successfully",
-            }, { status: 200 });
-        }
+      return NextResponse.json(
+        {
+          type: "SUCCESS",
+          message: "User updated successfully",
+        },
+        { status: 200 }
+      );
     }
+  } catch (error: any) {
     // errror handle
-    catch (error: any) {
-        console.log(error.message)
-        return NextResponse.json({
-            type: "ERROR",
-            message: error.message ||'Something went wrong!',
-        }, { status: 500 });
-    }
-}   
+    console.log(error.message);
+    return NextResponse.json(
+      {
+        type: "ERROR",
+        message: error.message || "Something went wrong!",
+      },
+      { status: 500 }
+    );
+  }
+}
 
-export async function GET(req:NextRequest) {  
-    const userHeader = req.headers.get("user");
-        const authUser: any = userHeader ? JSON.parse(userHeader) : null;
-        if(authUser?.role!=="admin" && authUser?.role!=="manager"){
-            return NextResponse.json({
-                type: "BAD_REQUEST",
-                message: "You are not authorized to create user",
-            }, { status: 403 });
-        }
-    try {
+export async function GET(req: NextRequest) {
+  const userHeader = req.headers.get("user");
+  const authUser: any = userHeader ? JSON.parse(userHeader) : null;
+  if (authUser?.role !== "admin" && authUser?.role !== "manager") {
+    return NextResponse.json(
+      {
+        type: "BAD_REQUEST",
+        message: "You are not authorized to create user",
+      },
+      { status: 403 }
+    );
+  }
+  try {
     const searchUrl = new URL(req.url);
-    const name = searchUrl.searchParams.get('name') || ''
-    const role = searchUrl.searchParams.get('role') || ''
-    const type = searchUrl.searchParams.get('type') || ''
-    const filterQuery: { name?: string | { $regex: string; $options: string }; role?: string; type?: string } = {};
-    if(name && name.trim()){
-        filterQuery.name = { $regex: name, $options: 'i' };
+    const name = searchUrl.searchParams.get("name") || "";
+    const role = searchUrl.searchParams.get("role") || "";
+    const type = searchUrl.searchParams.get("type") || "";
+    const filterQuery: {
+      name?: string | { $regex: string; $options: string };
+      role?: string;
+      type?: string;
+    } = {};
+    if (name && name.trim()) {
+      filterQuery.name = { $regex: name, $options: "i" };
     }
-    if(role && role.trim()){
-        filterQuery.role=role
+    if (role && role.trim()) {
+      filterQuery.role = role;
     }
-    if(type && type.trim()){
-        filterQuery.type=type
+    if (type && type.trim()) {
+      filterQuery.type = type;
     }
-        const users = await Users.find(filterQuery);
-        if (!users) {
-            return NextResponse.json({
-                type: "BAD_REQUEST",
-                message: "User not found",
-            }, { status: 404 });
-        }
-        return NextResponse.json(users);
-    } catch (error) {
-        return NextResponse.json({
-            type: "BAD_REQUEST",
-            message: "Something went wrong!"
-         }, { status: 500 });
+    const users = await Users.find(filterQuery);
+    if (!users) {
+      return NextResponse.json(
+        {
+          type: "BAD_REQUEST",
+          message: "User not found",
+        },
+        { status: 404 }
+      );
     }
+    return NextResponse.json(users);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        type: "BAD_REQUEST",
+        message: "Something went wrong!",
+      },
+      { status: 500 }
+    );
+  }
 }
 
 // export async function GET() {
@@ -172,13 +204,12 @@ export async function GET(req:NextRequest) {
 //             };
 //         }));
 
-//         return NextResponse.json(result); 
+//         return NextResponse.json(result);
 //     } catch (error) {
 //         // console.error(error);
 //         return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
 //     }
 // }
-
 
 // export async function PUT(req:NextRequest) {
 //     try {
@@ -233,4 +264,3 @@ export async function GET(req:NextRequest) {
 //     }
 // }
 // Initialize ImageKit
-
